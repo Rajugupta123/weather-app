f45747fdc1e798489f5db844142be0db9c0ef763
Rajugupta123 <raju.kgupta77@gmail.com>
2023-07-26 17:49:10 +0545

    weather search

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const cityField = document.querySelector(".weather2 p");
 const dateField = document.querySelector(".weather2 span");
 const emojiField = document.querySelector(".weather3 img");
 const weatherField = document.querySelector(".weather3 span");
-
 const searchField = document.querySelector(".searchField");
 const form = document.querySelector("form");
 
+//default location
 let target = "Kathmandu"
 
-const fetchData=async()=>{
-    const url = `https://api.weatherapi.com/v1/current.json?key=5b27a6ef3547402582e62007222306&q=${target}`;
+//func to fetch data from weather api
+const fetchData=async(target)=>{
+    try {
+        const url = `https://api.weatherapi.com/v1/current.json?key=5b27a6ef3547402582e62007222306&q=${target}`;
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data); 
+    //console.log(data); 
 
     //destructuring
     const{
@@ -22,26 +24,45 @@ const fetchData=async()=>{
         location:{name,localtime}
     } = data;
 
+    //calling update dom function
     updateDom(temp_c,name,localtime,icon,text);
+    
+    }catch (error) {
+     alert("location not found");   
+    }
 }
+
+//updating the dom
 function updateDom (temp,city,time,emoji,wcondt){
+    const exactDate = time.split(" ")[0];
+    const exactTime = time.split(" ")[1];
+    const exactDay = new Date(exactDate).getDay();
+    const dayName = getFullDayName(exactDay);
+
     tempField.innerText = temp;
     cityField.innerText = city;
     //dateField.innerText = time;
-    console.log(time);
-    const exactDate = time.split(" ")[0];
-    const exactTime = time.split(" ")[1];
-    console.log(exactDate);
-    console.log(exactTime);
-    const exactDay = new Date(exactDate).getDay();
-    const dayName = getFullDayName(exactDay);
+    //console.log(time);
+    //console.log(exactDate);
+    //console.log(exactTime);
+    
     dateField.innerText = `${exactTime} ${dayName} ${exactDate}`;
 
     emojiField.src = emoji;
     weatherField.innerText =  wcondt;
 }
-fetchData();
+fetchData(target);
 
+//to search the location entered by user.
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    target = searchField.value;
+    // console.log(target);
+    fetchData(target);
+});
+
+
+//func to get name of day.
 function getFullDayName(num){
 
     switch(num){
@@ -70,12 +91,3 @@ function getFullDayName(num){
             return "don't know";
     }
 }
-
-
-
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    target = searchField.value;
-    // console.log(target);
-    fetchData();
-});
